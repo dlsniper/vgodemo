4356961a96dcb2b3a0d96c4db7caa9d24109a46b
Florin Patan <florinpatan@gmail.com>
2018-07-04 13:22:33 +0300

    Initial commit

---
+++ demo.go
@@ -0,0 +1,9 @@
+package vgodemo
+
+import (
+	"github.com/gorilla/mux"
+)
+
+func GetRouter() interface{} {
+	return mux.Router{}
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dlsniper/vgodemo
+
+require github.com/gorilla/mux v1.6.2
